1e15de044a88fb21ab9980eb0357adb706c881fb
00jup <jupark0369@gmail.com>
2023-11-10 17:02:40 +0900

    visual programming 10weeks

---
+++ 20231110/_test.py
@@ -0,0 +1,18 @@
+print('Coord : {y} {y} {x}'.format(x='444', y='222'))
+# print('Coord : {} {} {x}'.format(x='444', y='222'))
+
+print('{:*<30}'.format("hello world!"))
+print('{:*>30}'.format("hello world!"))
+print('{:30}'.format(11111111111111111111))
+# ppt '는 구분하려고 넣은 거?
+
+# f-string -> built in 나중에 나왔는데 왜?
+
+print(f'{3.14:+o}')
+print(f'{3.14:+f}')
+print(f'{1234567890:,}')
+print(f'{1234567890:#x}')
+
+
+bug = 'ddd'
+print(f"Debugging {bug=}{bug=}{bug}")
