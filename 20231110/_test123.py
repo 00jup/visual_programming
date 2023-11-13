7c3f73bbe7ca06f93d0862fe4caf1079f79b1c88
00jup <jupark0369@gmail.com>
2023-11-13 23:29:01 +0900

    python start" " "

---
+++ 20231110/_test123.py
@@ -0,0 +1,6 @@
+import torch
+import tensorflow as tf
+
+print(tf.__version__)
+print(f"MPS 장치를 지원하도록 build가 되었는가? {torch.backends.mps.is_built()}")
+print(f"MPS 장치가 사용 가능한가? {torch.backends.mps.is_available()}")
