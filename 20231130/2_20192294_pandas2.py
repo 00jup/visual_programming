fc1cfa79d7005dd0d5dd8bd3c638d87a3bb26d27
00jup <jupark0369@gmail.com>
2023-11-30 23:13:29 +0900

    pandas example

---
+++ 20231130/2_20192294_pandas2.py
@@ -0,0 +1,20 @@
+# 생존자 survived = 1과 사망자의 남녀 성비를 구하여라.
+import pandas as pd
+
+titanic = pd.read_csv("titanic.csv")
+
+print(titanic)
+
+row = titanic.loc[1, :]
+print(f"{row} \n")
+
+# survived_people = titanic.loc[:, ["Survived"]]
+survived_people = titanic.groupby("Sex")[["Survived"]].sum()
+
+for column in survived_people:
+    print(column)
+    print(survived_people[column])
+    print()
+
+print(survived_people.head(1))
+print(survived_people.tail(1))
